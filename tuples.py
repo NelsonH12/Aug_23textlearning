b02ac3fa6583bbd0c002ed9f176531d304af70a3
Nelson Hopp <108594613+NelsonH12@users.noreply.github.com>
2023-08-23 12:29:07 -0400

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,6 @@
+#tuples
+#tuples are immudable, can't be changed or modified.  Lists can be changed.
+coordinates = (4, 5) #xy coordinates
+print(coordinates[0]) #index position inside brackets, which starts at 0 not 1.
+#can create a list of tuples:
+coordinates = [(4, 5), (6, 7), (80, 34)]
